bf4aa31ac8683cf68f8618e1351a4b734262881f
PulgaGit <visit.pulga@gmail.com>
2025-04-14 12:58:26 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,1 @@
-// Example of a simple script you might want to add later for interactivity
-document.addEventListener("DOMContentLoaded", function () {
-    console.log("Modern Electrical CTM website is loaded!");
-});
+
